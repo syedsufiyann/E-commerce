f4ff7c532ef5b52008f97028c9c70476363c8f48
syedsufiyan888 <syedsufiyan4567@gmail.com>
2025-01-02 15:48:14 +0530

    improved functionality

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -1,10 +1,7 @@
 import CartCard from "./CartCard";
+import { Link } from "react-router-dom";
 const Cart = ({ cartItems, setCartItems }) => {
     console.log(cartItems.title);
-
-    if (cartItems.length === 0) {
-        return <h3 className="empty-cart">Your cart is empty! <br /> Please add items to the cart.</h3>
-    }
 
     const onUpdateQty = (productId, quantity) => {
         const updatedCartItems = cartItems.map((product) => {
@@ -20,19 +17,41 @@ const Cart = ({ cartItems, setCartItems }) => {
         const filterCartItems = cartItems.filter((product) => (product.id !== productId))
         setCartItems(filterCartItems)
     }
+
+    const getTotoal = () => {
+        return (
+            cartItems.reduce((acc, prod) => acc + prod.price * prod.quantity, 0)
+        )
+    }
+
+    if (cartItems.length === 0) {
+        return (
+            <div className="empty-cart">
+                <h3 >Your cart is empty! <br />
+                    Please add items to the cart.</h3>
+                <Link to="/" className="link">
+                    <button>Back to Shopping</button>
+                </Link>
+            </div>
+        )
+    }
     return (
-        cartItems.map((product) => {
-            return (
-
-                <CartCard
-                    product={product}
-                    key={product.id}
-                    onRemove={onRemove}
-                    onUpdateQty={onUpdateQty}
-                />
-            )
-
-        })
+        <div>
+            <div className="total-backbtn">
+                <Link to="/" className="link"><button><svg className="back-arrow" xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="17px" fill="#000000"><path d="m313-440 224 224-57 56-320-320 320-320 57 56-224 224h487v80H313Z" /></svg>Back</button></Link>
+                <p>Total <span className="t-price"> ${getTotoal().toFixed(2)}</span></p>
+            </div>
+            {cartItems.map((product) => {
+                return (
+                    <CartCard
+                        product={product}
+                        key={product.id}
+                        onRemove={onRemove}
+                        onUpdateQty={onUpdateQty}
+                    />
+                )
+            })}
+        </div>
     );
 }
 export default Cart;
--- src/Components/CartCard.jsx
+++ src/Components/CartCard.jsx
@@ -1,11 +1,20 @@
+import { useEffect } from "react";
+
 const CartCard = ({ product, onRemove, onUpdateQty }) => {
     const onhandleIncrement = () => {
         const currentQuantity = Number(product.quantity)||1;
         onUpdateQty(product.id, currentQuantity + 1);
     }
     const onhandleDecrement = () => {
-
+        if(product.quantity>1){
+            onUpdateQty(product.id,product.quantity-1)
+        }
     }
+    useEffect(() => {
+        if (isNaN(product.quantity)) {
+            onUpdateQty(product.id, 1); 
+        }
+    }, [product.id, product.quantity, onUpdateQty]);
     return (
         <>
             <div className="Cart-Products">
@@ -15,7 +24,7 @@ const CartCard = ({ product, onRemove, onUpdateQty }) => {
                     </div>
                     <div className="Cart-Price">
                         <h3 className="Cart-Title">{product.title}</h3>
-                        <p>Price: ${product.price}</p>
+                        <p>Price: ${(product.price*product.quantity).toFixed(2)}</p>
                         <div className="qty-btns">
                             <button onClick={onhandleDecrement} disabled={product.quantity <= 1}>-</button>
                             <span>{product.quantity}</span>
